3fe7bfc05eb34b4249521c0d15a841efe1aaff56
Doug A Carter <doug.allen.carter@gmail.com>
2018-04-13 22:23:10 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,1 +1,30 @@
-
+<div class="container mainContainer"> 
+    <h1>Home</h1>
+    
+ <!--Posts section-->   
+    <div class="row">
+        <div class="col-md-8">
+        
+            <h2>Recent Posts</h2>
+        
+            <?php displayPosts('public');?>
+            
+        </div>
+        
+        
+   <!--Search and Post Box-->     
+        <div class="col-md-4">
+        
+        <?php displaySearch(); ?>
+            <hr>
+        <?php displayPostBox(); ?>    
+        
+        </div>
+    </div>
+    
+    
+    
+    
+    
+    
+</div> 
